199cd51eb6d879ea62ce1926973107c1f9c94fb9
MoatazFahmy2003 <motaz.fahmy.hassan@gmail.com>
2024-12-29 17:36:01 +0200

    merge
    e Please enter the commit message for your changes. Lines starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-<<<<<<< HEAD
-import ProfilePage from './pages/ProfilePage';
-=======
-import CarPage from './pages/CarPage';
-import ReservationPage from './pages/ReservationPage';
-import CheckoutPage from './pages/CheckoutPage';
-import PaymentSuccessPage from './pages/PaymentSuccessPage';
->>>>>>> main
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import CarPage from "./pages/CarPage";
+import ReservationPage from "./pages/ReservationPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PaymentSuccessPage from "./pages/PaymentSuccessPage";
 
 function App() {
   return (
@@ -21,14 +18,14 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-<<<<<<< HEAD
         <Route path="/profile" element={<ProfilePage />} />
-=======
         <Route path="/cars/:plate_id" element={<CarPage />} />
         <Route path="/reserve/:plate_id" element={<ReservationPage />} />
         <Route path="/checkout/:orderNo" element={<CheckoutPage />} />
-        <Route path="/payment-success/:orderNo" element={<PaymentSuccessPage />} />
->>>>>>> main
+        <Route
+          path="/payment-success/:orderNo"
+          element={<PaymentSuccessPage />}
+        />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
